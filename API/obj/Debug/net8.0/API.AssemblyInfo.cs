cdb6f9caf3d0f1220fa1b285eebd67101be8f052
siboshvili <siboshvili@gmail.com>
2024-04-13 00:32:14 +0400

    Reading the data from the Database in the API

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d976c813f05640ffebb76f5c654c50a872d21598")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f044271c3a6c76e34110259b3f6bb5b12ce23be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
