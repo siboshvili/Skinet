8098586316f54ee86ea09fb56d2ef9d8dfe59d57
siboshvili <siboshvili@gmail.com>
2024-04-13 00:55:13 +0400

    Creating the additional projects

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f044271c3a6c76e34110259b3f6bb5b12ce23be6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdb6f9caf3d0f1220fa1b285eebd67101be8f052")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
