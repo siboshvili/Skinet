7ebf0ce8b8dac6c5cc523c2650d81abcd011b6db
siboshvili <siboshvili@gmail.com>
2024-04-13 09:53:22 +0400

    Adding a Repository and Interface

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8098586316f54ee86ea09fb56d2ef9d8dfe59d57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59249dbe8095b75ab61f67741e2939e1e45142b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
