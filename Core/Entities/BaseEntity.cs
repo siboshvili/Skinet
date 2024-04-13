4fd15bedb3e2518569a5bb817aadb82d40213b5b
siboshvili <siboshvili@gmail.com>
2024-04-13 11:45:57 +0400

    Extending the products entity and creating related entities

--- Core/Entities/BaseEntity.cs
+++ Core/Entities/BaseEntity.cs
@@ -2,6 +2,6 @@ namespace Core.Entities
 {
     public class BaseEntity
     {
-        public int MyProperty { get; set; }
+        public int Id { get; set; }
     }
 }
--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c0926c3cddea3e4d6736f57190abcaec2be90ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acffb4b3419edf54ed4f4bdf9aff8764520992d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
