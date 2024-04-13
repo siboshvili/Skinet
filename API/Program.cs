acffb4b3419edf54ed4f4bdf9aff8764520992d0
siboshvili <siboshvili@gmail.com>
2024-04-13 11:25:53 +0400

    Main branch

--- API/Program.cs
+++ API/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
