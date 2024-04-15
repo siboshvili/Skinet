df544b6fa4e70ee12e84cae9420c550bb0e2e176
siboshvili <siboshvili@gmail.com>
2024-04-15 21:31:05 +0400

    Creating Exception handler middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Data.Migrations;
@@ -24,6 +25,8 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
 if (app.Environment.IsDevelopment())
