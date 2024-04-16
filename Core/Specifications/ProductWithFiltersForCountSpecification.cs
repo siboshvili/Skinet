ecc411d704d0db52178dc760d7123eec83e96225
siboshvili <siboshvili@gmail.com>
2024-04-16 13:21:42 +0400

    Adding the search functionality

--- Core/Specifications/ProductWithFiltersForCountSpecification.cs
+++ Core/Specifications/ProductWithFiltersForCountSpecification.cs
@@ -5,7 +5,10 @@ namespace Core.Specifications
     public class ProductWithFiltersForCountSpecification : BaseSpecification<Product>
     {
         public ProductWithFiltersForCountSpecification(ProductSpecParams productParams) : base(
-            x => (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            x =>
+            (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+            (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+            (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
         )
         {
         }
--- Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
@@ -6,7 +6,10 @@ namespace Core.Specifications
     public class ProductWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
         public ProductWithTypesAndBrandsSpecification(ProductSpecParams productParams) : base(
-            x => (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            x =>
+            (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+            (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+            (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
         )
         {
             AddInclude(x => x.ProductType);
