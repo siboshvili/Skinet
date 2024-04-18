d5f85a28e8a07d8eec0288cd345fe598e91a2752
siboshvili <siboshvili@gmail.com>
2024-04-18 13:05:45 +0400

    Adding the product filters.

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../shared/models/product';
 import { ShopService } from './shop.service';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
 
 @Component({
   selector: 'app-shop',
@@ -9,12 +11,34 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: Product[] = [];
+  brands: Brand[] = [];
+  types: Type[] = [];
 
   constructor(private shopService: ShopService) {}
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
     this.shopService.getProducts().subscribe({
       next: (response) => (this.products = response.data),
+      error: (error) => console.log(error),
+    });
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe({
+      next: (response) => (this.brands = response),
+      error: (error) => console.log(error),
+    });
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe({
+      next: (response) => (this.types = response),
       error: (error) => console.log(error),
     });
   }
