af4b08b681e08505424b3eb69354bee25307b908
siboshvili <siboshvili@gmail.com>
2024-04-20 11:44:45 +0400

    Adding loading indicators.

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -8,6 +8,7 @@ import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 import { BreadcrumbComponent } from 'xng-breadcrumb';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { BreadcrumbComponent } from 'xng-breadcrumb';
       preventDuplicates: true,
     }),
     BreadcrumbComponent,
+    NgxSpinnerModule,
   ],
-  exports: [NavBarComponent, SectionHeaderComponent],
+  exports: [NavBarComponent, SectionHeaderComponent, NgxSpinnerModule],
 })
 export class CoreModule {}
