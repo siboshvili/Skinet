aae8b3ce53053f3f2b2924be3eaf647ece908d2c
siboshvili <siboshvili@gmail.com>
2024-04-20 10:28:35 +0400

    Setting the section header title from the breadcrumb service.

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -13,7 +14,8 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private shopService: ShopService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private bsService: BreadcrumbService
   ) {}
 
   ngOnInit(): void {
@@ -24,7 +26,10 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (id)
       this.shopService.getProduct(+id).subscribe({
-        next: (product) => (this.product = product),
+        next: (product) => {
+          this.product = product;
+          this.bsService.set('@productDetails', product.name);
+        },
         error: (error) => console.log(error),
       });
   }
