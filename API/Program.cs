a76a82f44bc87b7abe6e788d370b2ebbe5fddd06
siboshvili <siboshvili@gmail.com>
2024-04-23 10:56:53 +0400

    Adding the IdentityDbContext.

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AppApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
