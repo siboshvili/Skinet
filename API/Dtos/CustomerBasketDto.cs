2bbd8f52df710aa25d9afe484711ca62824b5847
siboshvili <siboshvili@gmail.com>
2024-04-25 20:00:22 +0400

    Validating the basket.

---
+++ API/Dtos/CustomerBasketDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using Core.Entities;
+
+namespace API.Dtos
+{
+    public class CustomerBasketDto
+    {
+
+        [Required]
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    }
+}
