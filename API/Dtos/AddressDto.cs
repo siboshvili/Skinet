fa326397fa7f80e0162feec81208296a8eae50d3
siboshvili <siboshvili@gmail.com>
2024-04-25 11:40:40 +0400

    Adding another Dto for the user.

--- API/Dtos/AddressDto.cs
+++ API/Dtos/AddressDto.cs
@@ -1,18 +1,14 @@
-using System.ComponentModel.DataAnnotations;
 
-namespace Core.Entities.Identity
+namespace API.Dtos
 {
-    public class Address
+    public class AddressDto
     {
-        public int Id { get; set; }
+
         public string FistName { get; set; }
         public string LastName { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string ZipCode { get; set; }
-        [Required]
-        public string AppUserId { get; set; }
-        public AppUser AppUser { get; set; }
     }
 }
