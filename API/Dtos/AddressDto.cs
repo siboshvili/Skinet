1e26ea6dc7223c2373ae7ed0397f27a9db14dcc9
siboshvili <siboshvili@gmail.com>
2024-04-25 19:37:02 +0400

    Checking for duplicate email addresses.

--- API/Dtos/AddressDto.cs
+++ API/Dtos/AddressDto.cs
@@ -1,14 +1,26 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class AddressDto
     {
-
+        [Required]
         public string FistName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
         public string Street { get; set; }
+
+        [Required]
         public string City { get; set; }
+
+        [Required]
         public string State { get; set; }
+
+        [Required]
         public string ZipCode { get; set; }
     }
 }
