af2ce5dad8e633dc4011c5661db1da00bf02780e
siboshvili <siboshvili@gmail.com>
2024-04-28 10:31:06 +0400

    Creating a register form.

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private router: Router
+  ) {}
 
+  complexPassword =
+    "(?=^.{8,20}$)(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*s).*$";
+
+  registerForm = this.fb.group({
+    displayName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [Validators.required, Validators.pattern(this.complexPassword)],
+    ],
+  });
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => this.router.navigateByUrl('/shop'),
+    });
+  }
 }
