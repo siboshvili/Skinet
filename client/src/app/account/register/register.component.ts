5624e05c54b570940dd6e5a565dd4c0f7f09b2c1
siboshvili <siboshvili@gmail.com>
2024-04-28 11:01:34 +0400

    Dealing with modelstate errors.

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -9,6 +9,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  errors: string[] | null = null;
+
   constructor(
     private fb: FormBuilder,
     private accountService: AccountService,
@@ -30,6 +32,7 @@ export class RegisterComponent {
   onSubmit() {
     this.accountService.register(this.registerForm.value).subscribe({
       next: () => this.router.navigateByUrl('/shop'),
+      error: (error) => (this.errors = error.errors),
     });
   }
 }
