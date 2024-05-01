dd5b6d2efcae54d0fa25166707e1e9a7d784595d
siboshvili <siboshvili@gmail.com>
2024-05-01 20:18:06 +0400

    Adding the checkout form.

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent {
+  constructor(private fb: FormBuilder) {}
 
+  checkoutForm = this.fb.group({
+    addressFrom: this.fb.group({
+      firstName: ['', Validators.required],
+      lasName: ['', Validators.required],
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipcode: ['', Validators.required],
+    }),
+    deliveryForm: this.fb.group({
+      deliveryMethod: ['', Validators.required],
+    }),
+    paymanetForm: this.fb.group({
+      nameOnCard: ['', Validators.required],
+    }),
+  });
 }
