1453a827fa7ae18a049eb499b65eed56057d81ca
siboshvili <siboshvili@gmail.com>
2024-05-03 22:07:58 +0400

    Populating the address form from API.

--- Core/Entities/Identity/Address.cs
+++ Core/Entities/Identity/Address.cs
@@ -5,11 +5,23 @@ namespace Core.Entities.Identity
     public class Address
     {
         public int Id { get; set; }
+
+        [Required]
         public string FistName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
         public string Street { get; set; }
+
+        [Required]
         public string City { get; set; }
+
+        [Required]
         public string State { get; set; }
+
+        [Required]
         public string ZipCode { get; set; }
         [Required]
         public string AppUserId { get; set; }
