87b8f62bfb9faa359db93a5e824174b605105192
siboshvili <siboshvili@gmail.com>
2024-05-05 14:47:50 +0400

    Stripe validation.

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -30,6 +30,9 @@ export class CheckoutPaymentComponent implements OnInit {
   cardNumber?: StripeCardNumberElement;
   cardExpiry?: StripeCardExpiryElement;
   cardCvc?: StripeCardCvcElement;
+  cardNumberComplete = false;
+  cardExpiryComplete = false;
+  cardCvcComplete = false;
   cardErrors: any;
   loading = false;
 
@@ -49,6 +52,7 @@ export class CheckoutPaymentComponent implements OnInit {
         this.cardNumber = elements.create('cardNumber');
         this.cardNumber.mount(this.cardNumberElement?.nativeElement);
         this.cardNumber.on('change', (event) => {
+          this.cardNumberComplete = event.complete;
           if (event.error) this.cardErrors = event.error.message;
           else this.cardErrors = null;
         });
@@ -56,6 +60,7 @@ export class CheckoutPaymentComponent implements OnInit {
         this.cardExpiry = elements.create('cardExpiry');
         this.cardExpiry.mount(this.cardExpiryElement?.nativeElement);
         this.cardExpiry.on('change', (event) => {
+          this.cardExpiryComplete = event.complete;
           if (event.error) this.cardErrors = event.error.message;
           else this.cardErrors = null;
         });
@@ -63,11 +68,21 @@ export class CheckoutPaymentComponent implements OnInit {
         this.cardCvc = elements.create('cardCvc');
         this.cardCvc.mount(this.cardCvcElement?.nativeElement);
         this.cardCvc.on('change', (event) => {
+          this.cardCvcComplete = event.complete;
           if (event.error) this.cardErrors = event.error.message;
           else this.cardErrors = null;
         });
       }
     });
+  }
+
+  get paymentFormComplete() {
+    return (
+      this.checkoutForm?.get('paymentForm')?.valid &&
+      this.cardNumberComplete &&
+      this.cardExpiryComplete &&
+      this.cardCvcComplete
+    );
   }
 
   async submitOrder() {
