c20fad3acf9a8cbe08ffa52717c0f8aa0b185355
siboshvili <siboshvili@gmail.com>
2024-05-05 14:25:46 +0400

    Enabling linear mode.

--- client/src/app/checkout/checkout-review/checkout-review.component.ts
+++ client/src/app/checkout/checkout-review/checkout-review.component.ts
@@ -19,7 +19,6 @@ export class CheckoutReviewComponent {
   createPaymentIntent() {
     this.basketService.createPaymentIntent().subscribe({
       next: () => {
-        this.toastr.success('Payment intent created');
         this.appStepper?.next();
       },
       error: (error) => this.toastr.error(error.message),
