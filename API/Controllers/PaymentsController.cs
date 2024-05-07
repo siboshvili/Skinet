8cee1d0c3b8b494d28ff4aa89342839ea12f32cc
siboshvili <siboshvili@gmail.com>
2024-05-07 12:15:19 +0400

    Pre deployment work.

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -10,13 +10,15 @@ namespace API.Controllers
 {
     public class PaymentsController : BaseApiController
     {
-        private const string WhSecret = "whsec_6ffbac616d34fbae42b49d91cb104fc0258446cde53b487c265bad8ebd35c32c";
+        private readonly string _whSecret;
         private readonly IPaymentService _paymentService;
         private readonly ILogger<PaymentsController> _logger;
-        public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger)
+        public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger, IConfiguration config)
         {
             _logger = logger;
             _paymentService = paymentService;
+            _whSecret = config.GetSection("StripeSettings:WhSecret").Value;
+
         }
 
         [Authorize]
@@ -37,7 +39,7 @@ namespace API.Controllers
             var json = await new StreamReader(Request.Body).ReadToEndAsync();
 
             var stripeEvent = EventUtility.ConstructEvent(json,
-                Request.Headers["Stripe-Signature"], WhSecret);
+                Request.Headers["Stripe-Signature"], _whSecret);
 
             PaymentIntent intent;
             Order order;
